f57fefd83bfbf5a6540e413f0ec3ffc1edd07035
Your Name <you@example.com>
2025-03-11 18:59:58 -0300

    Início de projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import BoasVindas from "./components/BoasVindas";
+import { CardDefault } from "./components/Card";
+import { Aluno, Turma } from "./components/Classe";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Olá Mundo!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BoasVindas
+        nome="Marcos"
+        turma="FS22"
+      />
+      <BoasVindas
+        nome="Nagila"
+        turma="FS22"
+      />
+      <BoasVindas
+        nome="Matheus"
+        turma="FS31"
+      />
+      <Turma nome={"FS22"}>
+        <Aluno nome={"Well"} />
+        <Aluno nome={"Eduardo"} />
+        <Aluno nome={"Leo"} />
+        <Aluno nome={"Marcos"} />
+      </Turma>
+      <Turma nome={"FS31"}>
+        <Aluno nome={"Matheus"} />
+        <Aluno nome={"Adriana"} />
+        <Aluno nome={"Isaac"} />
+        <Aluno nome={"Magnum"} />
+      </Turma>
+
+      <CardDefault 
+        urlImagem={"https://cdn.motor1.com/images/mgl/KPK4R/s3/bmw-320i-m-sport-2021-teste-br.jpg"}
+        titulo={"Meu titulo"}
+        descricao={"lorem ipsum dolor"}
+      />
+      <CardDefault 
+        urlImagem={"https://img.freepik.com/fotos-premium/ponte-iluminada-do-bosforo-a-noite-istambul-turquia_400112-629.jpg?w=360"}
+        titulo={"Istambul Turquia"}
+        descricao={"Ponte iluminada do Bósforo"}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
